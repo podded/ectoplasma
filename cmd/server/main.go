470d24540f12a0f9b0d71fc95b67a2439c7cefe2
CryptaEve <crypta@crypta.tech>
2020-03-08 16:24:50 +1100

    Deconflict server ports

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	// Just start up the minimal server to put things onto the ingest queue
 	// TODO Make all of this configurable!
-	goop := podgoo.NewPodGoo("http://localhost:13270", 10*time.Second, "ecto_server")
+	goop := podgoo.NewPodGoo("http://localhost:13271", 10*time.Second, "ecto_server")
 	goop.BoundHost = "0.0.0.0"
 	goop.BoundPort = 13271
 
